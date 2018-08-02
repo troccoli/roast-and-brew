6f61ebd63f164f016aed8fcfd0f17d590fabd8c4
Giulio Troccoli-Allard <giulio@troccoli.it>
2018-08-02 15:15:27 +0100

    Added the app and login views (the only two vies we will need)

---
+++ resources/views/login.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Roast</title>
+
+</head>
+<body>
+<a href="/login/google">
+    Log In With Google
+</a>
+</body>
+</html>
