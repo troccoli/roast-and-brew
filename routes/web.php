2c40c1df31c21bd3ac478aba45cef1590958aa9c
Giulio Troccoli-Allard <giulio@troccoli.it>
2018-08-02 15:53:03 +0100

    Setup the loing page for Google+ and the user table

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,13 @@
 
 Route::get('/', 'Web/AppController@getApp')
     ->middleware('auth');
+
+Route::get('/login', 'Web\AppController@getLogin' )
+    ->name('login')
+    ->middleware('guest');
+
+Route::get( '/login/{social}', 'Web\AuthenticationController@getSocialRedirect' )
+    ->middleware('guest');
+
+Route::get( '/login/{social}/callback', 'Web\AuthenticationController@getSocialCallback' )
+    ->middleware('guest');
