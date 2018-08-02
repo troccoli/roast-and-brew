c1a2f8d6327ebf92b7b78ce4e1c47ea85eb3e153
Giulio Troccoli-Allard <giulio@troccoli.it>
2018-08-02 15:17:06 +0100

    Versioned the app.css and app.js files

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link href="/css/app.css" rel="stylesheet" type="text/css"/>
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" type="text/css"/>
 
     <link rel="icon" type="image/x-icon" href="/favicon.ico">
 
@@ -22,7 +22,7 @@
     <router-view></router-view>
 </div>
 
-<script type="text/javascript" src="js/app.js"></script>
+<script type="text/javascript" src="{{ mix('/js/app.js') }}"></script>
 
 </body>
 </html>
