db591ad9fc612f546807933083679b3319247dc2
Giulio Troccoli-Allard <giulio@troccoli.it>
2018-08-02 15:17:21 +0100

    Added the stylesheet to the login view

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" type="text/css"/>
     <title>Roast</title>
 
 </head>
