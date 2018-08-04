0b68253bbc1c5adaba71bcc8fd5840212d83570b
Giulio Troccoli-Allard <giulio@troccoli.it>
2018-08-04 17:39:30 +0100

    Added Vue Router and Vuex and some scaffolding

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -30,12 +30,12 @@ if (token) {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
+import Vue from "vue";
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-    el: '#app'
-});
+new Vue().$mount('#app');
