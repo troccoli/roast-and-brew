9b6e4e4de871e95e5ff6efb1d13a61e8d370554f
Giulio Troccoli-Allard <giulio@troccoli.it>
2018-08-05 17:36:20 +0100

    Setup the data store with Vuex

---
+++ resources/assets/js/store.js
@@ -0,0 +1,11 @@
+// Adds the promise polyfill for IE 11
+require('es6-promise').polyfill();
+
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    modules: {}
+});
