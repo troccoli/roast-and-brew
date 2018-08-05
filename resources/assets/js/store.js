004afd0ff6dd45a52baa0d8d54bf47f60f95531a
Giulio Troccoli-Allard <giulio@troccoli.it>
2018-08-05 17:54:15 +0100

    Added the cafes module to the Vuex store

--- resources/assets/js/store.js
+++ resources/assets/js/store.js
@@ -3,9 +3,12 @@ require('es6-promise').polyfill();
 
 import Vue from 'vue';
 import Vuex from 'vuex';
+import {cafes} from './modules/cafes.js';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    modules: {}
+    modules: {
+        cafes
+    }
 });
