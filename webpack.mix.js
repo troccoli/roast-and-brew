f8f72cf768915d62eba0d087c77cea75d93aad65
Giulio Troccoli-Allard <giulio@troccoli.it>
2018-08-06 15:38:11 +0100

    Added some initial style and cofigure teh VueJS app to use the SASS files

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,13 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css')
-    .version();
+mix.js('resources/assets/js/app.js', 'public/js'
+    .webpackConfig({
+        resolve: {
+            alias: {
+                '@': path.resolve('resources/assets/sass')
+            }
+        }
+    })
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .version()
