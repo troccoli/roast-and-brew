eb25a3958f9e284101c1858e6a16f3cd156b4247
Giulio Troccoli-Allard <giulio@troccoli.it>
2018-08-09 15:45:22 +0100

    Added a card for each cafe in the home page

---
+++ resources/assets/js/components/cafes/CafeCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="large-3 medium-4 small-6 cell cafe-card-container" v-show="show">
+        <router-link :to="{ name: 'cafe', params: { id: cafe.id} }">
+            <div class="cafe-card">
+                <span class="title">{{ cafe.name }}</span>
+                <span class="address">
+                    <span class="street">{{ cafe.address }}</span>
+                    <span class="city">{{ cafe.city }}</span>
+                    <span class="state">{{ cafe.state }}</span>
+                    <span class="zip">{{ cafe.zip }}</span>
+                </span>
+            </div>
+        </router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['cafe'],
+
+        data() {
+            return {
+                show: true
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/assets/js/pages/Home.vue
+++ resources/assets/js/pages/Home.vue
@@ -1,17 +1,34 @@
 <template>
     <div id="home">
-        <span v-show="cafesLoadStatus == 1">Loading</span>
-        <span v-show="cafesLoadStatus == 2">Cafes loaded successfully!</span>
-        <span v-show="cafesLoadStatus == 3">Cafes loaded unsuccessfully!</span>
-
-        <ul>
-            <li v-for="cafe in cafes">{{ cafe.name }}</li>
-        </ul>
+        <div class="grid-container">
+            <div class="grid-x grid-padding-x">
+                <loader v-show="cafesLoadStatus == 1" :width="100" :height="100"></loader>
+                <cafe-card v-for="cafe in cafes" :key="cafe.id" :cafe="cafe"></cafe-card>
+            </div>
+        </div>
     </div>
+
+    <!--<div id="home">-->
+        <!--<span v-show="cafesLoadStatus == 1">Loading</span>-->
+        <!--<span v-show="cafesLoadStatus == 2">Cafes loaded successfully!</span>-->
+        <!--<span v-show="cafesLoadStatus == 3">Cafes loaded unsuccessfully!</span>-->
+
+        <!--<ul>-->
+            <!--<li v-for="cafe in cafes">{{ cafe.name }}</li>-->
+        <!--</ul>-->
+    <!--</div>-->
 </template>
 
 <script>
+    import CafeCard from '../components/cafes/CafeCard';
+    import Loader from '../components/global/Loader';
+
     export default {
+        components: {
+            Loader,
+            CafeCard
+        },
+
         created() {
             this.$store.dispatch('loadCafes');
         },
