90eb71603f2626525934acac3da909f686ee27bc
Giulio Troccoli-Allard <giulio@troccoli.it>
2018-08-09 15:06:36 +0100

    Changed the marker used for cafes on the map

--- resources/assets/js/components/cafes/CafeMap.vue
+++ resources/assets/js/components/cafes/CafeMap.vue
@@ -61,16 +61,21 @@
             buildMarkers() {
                 this.markers = [];
 
-                for (var i = 0; i < this.cafes.length; i++) {
+                // Create the marker for each of the cafes and set the latitude and longitude to the latitude and
+                // longitude of the cafe. Also set the map to be the local map.
+                let image = '/img/coffee-marker.png';
+
+                for (let i = 0; i < this.cafes.length; i++) {
 
                     // Create the marker for each of the cafes and set the latitude and longitude to the latitude and
                     // longitude of the cafe. Also set the map to be the local map.
-                    var marker = new google.maps.Marker({
+                    let marker = new google.maps.Marker({
                         position: {
                             lat: parseFloat(this.cafes[i].latitude),
                             lng: parseFloat(this.cafes[i].longitude)
                         },
-                        map     : this.map
+                        map     : this.map,
+                        icon    : image
                     });
 
                     this.markers.push(marker);
