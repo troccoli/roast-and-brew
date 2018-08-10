882a880672787f78961d11ab1f74b02bbb851430
Giulio Troccoli-Allard <giulio@troccoli.it>
2018-08-10 16:15:23 +0100

    Added the parent-child relationship to the Cafe model

--- app/Models/Cafe.php
+++ app/Models/Cafe.php
@@ -13,4 +13,14 @@ class Cafe extends Model
         // Need to specify the table name as the default wold be 'brew_method_cafe'
         return $this->belongsToMany(BrewMethod::class, 'cafes_brew_methods');
     }
+
+    public function children()
+    {
+        return $this->hasMany(self::class, 'parent', 'id');
+    }
+
+    public function parent()
+    {
+        return $this->hasOne(self::class, 'id', 'parent');
+    }
 }
