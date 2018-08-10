948d4d83e3866249f0194536cfa0ef2e53a9261a
Giulio Troccoli-Allard <giulio@troccoli.it>
2018-08-10 16:10:37 +0100

    Added few fiedls to the cafes table to implement parent-child relationship

---
+++ database/migrations/2018_08_10_145712_added_cafe_parent_child_relationship.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddedCafeParentChildRelationship extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cafes', function (Blueprint $table) {
+            $table->unsignedInteger('parent')->nullable();
+            $table->string('location_name')->default('');
+            $table->integer('roaster')->nullable();
+            $table->text('website')->default('');
+            $table->text('description')->default('');
+            $table->unsignedInteger('added_by')->nullable();
+
+            $table->foreign('parent')->references('id')->on('cafes');
+            $table->foreign('added_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cafes', function (Blueprint $table) {
+            $table->dropColumn(['parent', 'location_name', 'roaster', 'website', 'description', 'added_by']);
+        });
+    }
+}
